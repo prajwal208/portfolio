59ec3113d4354633d417602c0437e0f8967f6259
Pr@jwal <76072862+prajwal208@users.noreply.github.com>
2022-05-15 21:18:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+AOS.init()
+
+AOS.init({
+    duration: 1600,
+})
+
+//Responsive-Menu
+
+const openmenu = () => {
+    document.getElementById('res-menu').style.width="250px"
+}
+
+const closemenu = () => {
+    document.getElementById('res-menu').style.width="0px"
+}
+
+
+
+
+
+
+
